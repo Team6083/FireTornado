288f59c66c22b9d63bf227200b2cbffa201012a8
Leafoliage <leafoliages@gmail.com>
2020-05-04 21:47:20 +0800

    [add] user schema read function

--- model/user.go
+++ model/user.go
@@ -1,6 +1,8 @@
 package model
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"gopkg.in/mgo.v2/bson"
+)
 
 type User struct {
 	Name            string        `json:"name"`
@@ -8,4 +10,31 @@ type User struct {
 	Password        string        `json:"password"`
 	Id              bson.ObjectId `bson:"id, omitempty" json:"id"`
 	PermissionLevel int           `json:"permission_level"`
+}
+
+func (database *Database) GetUserById(id string) (*User, error) {
+	user := User{}
+	err := database.DB.C("users").FindId(bson.ObjectIdHex(id)).One(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (database *Database) GetUserByName(name string) (*User, error) {
+	user := User{}
+	err := database.DB.C("users").Find(bson.M{"name": name}).One(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (database *Database) GetAllUsers() ([]User, error) {
+	var users []User
+	err := database.DB.C("users").Find(bson.M{}).All(&users)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
