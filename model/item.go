1bc3e49d3531575ce239b005372038cd6a4f95eb
Leafoliage <leafoliages@gmail.com>
2020-05-05 21:16:44 +0800

    [add] item read function

--- model/item.go
+++ model/item.go
@@ -1,5 +1,7 @@
 package model
 
+import "gopkg.in/mgo.v2/bson"
+
 type Item struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
@@ -9,4 +11,22 @@ type Item struct {
 	Unit        string `json:"unit"`
 	StoragePos  string `json:"storage_pos"`
 	Status      int    `json:"status"`
+}
+
+func (database *Database) GetItemByName(name string) (*Item, error) {
+	item := Item{}
+	err := database.DB.C("items").Find(bson.M{"name": name}).One(&item)
+	if err != nil {
+		return nil, err
+	}
+	return &item, nil
+}
+
+func (database *Database) GetAllItem() ([]Item, error) {
+	var items []Item
+	err := database.DB.C("items").Find(bson.M{}).All(&items)
+	if err != nil {
+		return nil, err
+	}
+	return items, err
 }
