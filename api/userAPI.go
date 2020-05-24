0a947a356cb53c746f1a04771da4f305e1e5782a
Leafoliage <leafoliages@gmail.com>
2020-05-24 10:37:48 +0800

    [add] userAPI

---
+++ api/userAPI.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"FireTornado/model"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func (web *Web) RouteHandler(router gin.Engine) {
+	usersGroup := router.Group("/user")
+	usersGroup.GET("/", web.APIReadUserById)
+	usersGroup.POST("/", web.APICreateUser)
+	usersGroup.PUT("/", web.APIUpdateUser)
+	usersGroup.DELETE("/", web.APIDeleteUser)
+}
+
+func (web *Web) APICreateUser(c *gin.Context) {
+	user := model.User{Id: bson.NewObjectId()}
+
+	err := c.ShouldBind(&user)
+	if err != nil {
+		panic(err) // TODO: don't use panic
+		return
+	}
+
+	create, err := web.DB.SaveUser(user)
+	if err != nil {
+		panic(err) // TODO: don't use panic
+		return
+	}
+
+	c.JSON(201, create)
+}
+
+func (web *Web) APIReadUserById(c *gin.Context) {
+	id := c.Query("id")
+
+	read, err := web.DB.GetUserById(id)
+	if err != nil {
+		panic(err) // TODO: don't use panic
+		return
+	}
+
+	c.JSON(200, read)
+}
+
+func (web *Web) APIUpdateUser(c *gin.Context) {
+	id := c.Query("id")
+
+	user := model.User{Id: bson.ObjectIdHex(id)}
+
+	err := c.ShouldBind(&user)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	update, err := web.DB.SaveUser(user)
+	if err != nil {
+		panic(err) // TODO: don't use panic
+		return
+	}
+
+	c.JSON(200, update)
+}
+
+func (web *Web) APIDeleteUser(c *gin.Context) {
+	id := c.Query("id")
+
+	err := web.DB.DeleteUser(model.User{Id: bson.ObjectIdHex(id)})
+	if err != nil {
+		panic(err) // TODO: don't use panic
+		return
+	}
+
+	c.Writer.WriteHeader(204)
+}
